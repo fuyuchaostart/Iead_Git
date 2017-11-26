6e0326fd84754c5556a1a0ee079f6fc44f9eef63
fycstart <fycstart@126.com>
2017-11-26 14:37:06 +0800

    Idea test

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @author fycstart 邮箱: fycstart@126.com
+ * @version 2017年11月26
+ *          天变不足畏，祖宗不足法，人言不足恤
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("折剑出江湖--温华");
+        System.out.println("折剑出江湖--温华");
+        System.out.println("折剑出江湖--温华");
+        System.out.println("折剑出江湖--温华");
+    }
+}
